5d9443705916197f318ed0c3cf14a1603432d5dd
dhafer <dhaferhermi1997@gmail.com>
2022-02-09 02:59:12 +0100

    searchAccount

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -5,6 +5,9 @@ namespace App\Controller;
 use App\Form\UserEditType;
 use App\Form\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SearchType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -23,7 +26,7 @@ class AccountController extends AbstractController
         $editForm = $this->createForm(UserEditType::class,$user);
         $editForm->handleRequest($req);
 
-        if ($editForm->isSubmitted() && $editForm->isValid()){
+        if ($editForm->isSubmitted() and $editForm->isValid()){
             $editData=$editForm->getData();
             $em->persist($editData);
             $em->flush();
@@ -40,15 +43,47 @@ class AccountController extends AbstractController
      */
     public function viewAccount(): Response
     {
-
-
         $em = $this->getDoctrine()->getManager();
         $un = $this->get('security.token_storage')->getToken()->getUser()->getUsername();
-
         $user = $em->getRepository("App\Entity\User")->findOneBy(['username'=>$un]);
-
         return $this->render('account/viewAccount.html.twig', [
             'user' => $user
+        ]);
+    }
+
+    /**
+     * @Route("/searchAccount", name="searchAccount")
+     */
+    public function searchAccount(Request $request): Response
+    {
+        $form = $this->createFormBuilder()
+            ->add('keyword', TextType::class)
+            ->add('search', SubmitType::class)
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->get('keyword')->getData();
+            return $this->redirectToRoute('viewAccountSearchResults',['keyword'=>$data]);
+        }
+
+        return $this->render('account/searchAccount.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/viewAccountSearchResults/{keyword}", name="viewAccountSearchResults")
+     */
+    public function viewAccountSearchResults(Request $request,$keyword): Response
+    {
+        $em = $this->getDoctrine()->getManager();
+        $users = $em->getRepository("App\Entity\User")->findBy(array('lastname'=>$keyword));
+        $users = array_merge( $users,$em->getRepository("App\Entity\User")->findBy(array('firstname'=>$keyword)));
+        $users = array_merge( $users,$em->getRepository("App\Entity\User")->findBy(array('username'=>$keyword)));
+        return $this->render('account/viewAccountSearchResults.html.twig', [
+            'users'=>$users
         ]);
     }
 }
